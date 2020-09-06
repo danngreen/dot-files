509fa39d72a482e1b8257730a0330b9471dcdc96
Dan Green <danngreen1@gmail.com>
2020-09-06 14:55:23 -0700

    fzf uses fd instead of git ls-files

--- fzf.zsh
+++ fzf.zsh
@@ -17,10 +17,11 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 #fzf options thanks to https://github.com/samoshkin/dotfiles/blob/master/variables.sh
 export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:hidden:wrap' --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy),ctrl-x:execute(rm -i {+})+abort'"
 FD_OPTIONS="--hidden --follow --exclude .git --exclude node_modules"
-export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard || fd --type f --type l $FD_OPTIONS"
+#export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard || fd --type f --type l $FD_OPTIONS"
+export FZF_DEFAULT_COMMAND="fd --type f --type l $FD_OPTIONS"
 export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
 export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
-
+#fd --type f --type l --hidden --follow --exclude .git --exclude node_modules
 export BAT_PAGER="less -R"
 export BAT_THEME="Monokai Extended"
 
