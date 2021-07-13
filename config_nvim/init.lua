9c0b4532fdebbc5c09761a3330eb777bb5954800
Dan Green <danngreen1@gmail.com>
2021-07-13 14:05:28 -0700

    F10 = toggle big qf

--- config_nvim/init.lua
+++ config_nvim/init.lua
@@ -106,8 +106,18 @@ tnoremap('<F20>o', '<cmd>FloatermUpdate --position=topright --width=0.5 --height
 tnoremap('<F20>O', '<cmd>FloatermUpdate --position=topright --width=0.25 --height=0.25<CR>')
 
 noremap('<F9>', ':set list!<CR>')
-noremap('<F10>', ':Cope<CR>')
-noremap('<F22>', ':ccl<CR>')
+
+vim.cmd[[
+function! ToggleQuickFix()
+    if empty(filter(getwininfo(), 'v:val.quickfix'))
+        Cope | ccl | vertical botright cope 120
+    else
+        cclose
+    endif
+endfunction
+]]
+noremap('<F10>', ':call ToggleQuickFix()<CR>')
+
 noremap('<F11>', ':TagbarToggle<CR>')
 noremap('<F23>', ':Dispatch! ctags -R .<CR>')
 noremap('<F12>', ':NERDTreeToggle<CR>')
