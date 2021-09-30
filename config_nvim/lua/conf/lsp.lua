406657769c4048c2805cd6e2420e2b923d4977dc
Dan Green <danngreen1@gmail.com>
2021-09-29 21:37:13 -0700

    cmp scroll back/fwd with C-b/C-f

--- config_nvim/lua/conf/lsp.lua
+++ config_nvim/lua/conf/lsp.lua
@@ -22,7 +22,7 @@ cmp.setup{
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	  ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
 	  ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
