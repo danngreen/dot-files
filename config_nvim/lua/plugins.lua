eada058798202eae63bc742fb45f9d26ea108056
Dan Green <danngreen1@gmail.com>
2021-10-09 14:33:32 -0700

    FzfLua colors and options tweaked

--- config_nvim/lua/plugins.lua
+++ config_nvim/lua/plugins.lua
@@ -33,9 +33,49 @@ require('packer').startup(function()
 	use { 'ibhagwan/fzf-lua',
 		requires = {'vijaymarupudi/nvim-fzf' }, --, 'kyazdani42/nvim-web-devicons' } -- optional for icons
 		config = function()
-			-- local actions = require "fzf-lua.actions"
+			local monokaicolor = function(name)
+				local hex = require'monokai'.classic[name]
+				return hex:gsub("#(%x%x)(%x%x)(%x%x)", "0x%1,0x%2,0x%3")
+			end
 			require'fzf-lua'.setup {
-			  fzf_bin = 'sk',
+				fzf_bin = 'sk',
+				winopts = {
+				  fullscreen = true,
+				},
+				fzf_opts = {
+					['--layout'] = 'default', --vs. 'reverse'
+				},
+				fzf_colors = {
+					["fg"] = {"fg", "Normal"},
+				 	["bg"] = { "bg", "Normal" },
+					["hl"] = { "fg", "Keyword"}, --"matched"
+					["matched_bg"] = {"bg", "Normal"},
+					["fg+"] = { "fg", "Pmenu" }, --"fg of current line"
+					["bg+"] = { "fg", "LineNr" }, --"bg of current line"
+					["hl+"] = { "fg", "Keyword" }, --"current_match"
+					["current_match_bg"] = {"fg", "LineNr"},
+					--query
+					--query_bg
+					["info"] = { "fg", "PreProc" },
+					["prompt"] = { "fg", "Conditional" },
+					["pointer"] = { "fg", "Exception" },
+					["marker"] = { "fg", "Keyword" },
+					["spinner"] = { "fg", "Label" },
+					["header"] = { "fg", "Comment" },
+					["gutter"] = { "bg", "Normal" },
+				},
+				grep = {
+				--rg --colors [path,line,match,column]:[fg,bg,style]:[color|bold,nobold,underline,nounderline]
+				rg_opts="--hidden --column --line-number --no-heading " ..
+						"--color=always --smart-case " ..
+				 		"--colors 'path:fg:"..monokaicolor("aqua").."' "..
+				 		"--colors 'path:style:bold' "..
+				 		"--colors 'match:style:underline' "..
+						"-g '!{.git,node_modules}/*' ",
+					prompt            = 'Rg❯ ',
+					input_prompt      = 'Grep For❯ ',
+					actions           = { ["ctrl-q"] = false },
+				},
 			}
 		end,
 	}
