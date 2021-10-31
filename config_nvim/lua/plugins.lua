2b28cf9c4564dd2e9fa1172c25d466b6e365779d
Dan Green <danngreen1@gmail.com>
2021-10-31 15:06:37 -0700

    Added cmp-calc and dictionary completion

--- config_nvim/lua/plugins.lua
+++ config_nvim/lua/plugins.lua
@@ -76,12 +76,13 @@ require("packer").startup(
 			"hrsh7th/nvim-cmp",
 			requires = {
 				"hrsh7th/vim-vsnip",
-				--'L3MON4D3/LuaSnip'
-				--"saadparwaiz1/cmp_luasnip",
 				"hrsh7th/cmp-nvim-lua",
 				"hrsh7th/cmp-buffer",
 				"hrsh7th/cmp-nvim-lsp",
-				"hrsh7th/cmp-path"
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-calc",
+				--"lukas-reineke/cmp-rg",
+				"uga-rosa/cmp-dictionary",
 			}
 		}
 		use {
