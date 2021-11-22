ec035c4b37e34f85251ce2ce81ee6e12c716221e
Dan Green <danngreen1@gmail.com>
2021-11-22 11:14:54 -0800

    Fixed fish cursor in vi mode

--- config_fish/config.fish
+++ config_fish/config.fish
@@ -1,8 +1,8 @@
-if status is-interactive
-    set fish_plugins vi-mode
-    set fish_pager_color_prefix red --italics
-    set fish_pager_color_selected_background --background=666
-end
+# if status is-interactive
+# end
+set fish_plugins vi-mode
+set fish_pager_color_prefix red --italics
+set fish_pager_color_selected_background --background=666
 
 set EDITOR nvim
 set VISUAL nvim
@@ -13,7 +13,6 @@ fish_add_path /usr/local/opt/make/libexec/gnubin
 fish_add_path $HOME/4ms/stm32/gcc-arm-none-eabi-10-2020-q4-major/bin
 fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
 fish_add_path $HOME/Library/Python/3.9/bin
-fish_add_path $HOME/Library/Python/3.8/bin
 fish_add_path $HOME/bin
 fish_add_path /usr/local/bin
 fish_add_path $HOME/.espressif/tools/xtensa-esp32-elf/esp-2019r2-8.2.0/xtensa-esp32-elf/bin
@@ -29,14 +28,19 @@ set -x METAMODULE_ARTWORK_DIR $HOME/4ms/stm32/meta-module/vcv/res
 set -x METAMODULE_INFO_DIR $HOME/4ms/stm32/meta-module/shared/CoreModules/info
 set -x METAMODULE_PNG_DIR $HOME/4ms/stm32/meta-module/firmware/src/pages/images
 
-function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+function fish_key_hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
     for mode in default insert visual
         fish_default_key_bindings -M $mode
     end
-    fish_vi_key_bindings --no-erase
+    fish_vi_key_bindings --no-erase insert
 end
-set -g fish_key_bindings hybrid_bindings
+set -g fish_key_bindings fish_key_hybrid_bindings
 
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+set fish_vi_force_cursor
 
 function cd..
 	cd ..
@@ -62,7 +66,5 @@ function bind_dollar
     end
 end
 
-function fish_user_key_bindings
-    bind ! bind_bang
-    bind '$' bind_dollar
-end
+bind '$' bind_dollar
+bind '!' bind_bang
