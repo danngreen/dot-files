ba69a617d9ae756769f1ca7e50d6b875e032b172
Dan Green <danngreen1@gmail.com>
2021-11-22 09:33:11 -0800

    Config.fish formatting and trying hybrid_bindings

--- config_fish/config.fish
+++ config_fish/config.fish
@@ -1,8 +1,7 @@
 if status is-interactive
-	set fish_plugins vi-mode
-
-	set fish_pager_color_prefix red --italics
-	set fish_pager_color_selected_background --background=666
+    set fish_plugins vi-mode
+    set fish_pager_color_prefix red --italics
+    set fish_pager_color_selected_background --background=666
 end
 
 set EDITOR nvim
@@ -18,8 +17,8 @@ fish_add_path $HOME/Library/Python/3.8/bin
 fish_add_path $HOME/bin
 fish_add_path /usr/local/bin
 fish_add_path $HOME/.espressif/tools/xtensa-esp32-elf/esp-2019r2-8.2.0/xtensa-esp32-elf/bin
-fish_add_path $HOME/.local/bin"
-fish_add_path /usr/local/opt/llvm/bin"
+fish_add_path $HOME/.local/bin
+fish_add_path /usr/local/opt/llvm/bin
 
 set -x IDF_PATH $HOME/4ms/esp/esp-idf_4.0.1
 
@@ -30,10 +29,24 @@ set -x METAMODULE_ARTWORK_DIR $HOME/4ms/stm32/meta-module/vcv/res
 set -x METAMODULE_INFO_DIR $HOME/4ms/stm32/meta-module/shared/CoreModules/info
 set -x METAMODULE_PNG_DIR $HOME/4ms/stm32/meta-module/firmware/src/pages/images
 
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings hybrid_bindings
+
+
+function cd..
+	cd ..
+end
+
 function bind_bang
     switch (commandline -t)[-1]
         case "!"
-            commandline -t $history[1]; commandline -f repaint
+            commandline -t $history[1]
+            commandline -f repaint
         case "*"
             commandline -i !
     end
@@ -53,4 +66,3 @@ function fish_user_key_bindings
     bind ! bind_bang
     bind '$' bind_dollar
 end
-
