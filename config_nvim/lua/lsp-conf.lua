e83f4983e7a9955d75fab6868180173511adb2a0
Dan Green <danngreen1@gmail.com>
2022-10-23 10:24:31 -0700

    cmp_lsp: update_caps => default_caps

--- config_nvim/lua/lsp-conf.lua
+++ config_nvim/lua/lsp-conf.lua
@@ -14,7 +14,8 @@ local useccls = true
 -- Completion
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+-- capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Diagnostics
 
