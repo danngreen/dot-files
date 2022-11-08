d3b031f0ec2eafc7dda326ad2645a4217b69f3e5
Dan Green <danngreen1@gmail.com>
2022-11-08 09:16:05 -0800

    hilight for current search

--- config_nvim/lua/custom-hi.lua
+++ config_nvim/lua/custom-hi.lua
@@ -42,4 +42,6 @@ hi LspSignatureActiveParameter guifg=red
 hi! LspReferenceText guibg=#433536
 
 hi Pmenu guibg=#434546 
+
+hi CurSearch ctermfg=0 ctermbg=11 guifg=#f8f8f0 guibg=#fd971f gui=bold
 ]]
