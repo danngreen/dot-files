4f39c46a8e3349f11c2bd77b94d513291b35c4fb
Dan Green <danngreen1@gmail.com>
2022-12-12 21:31:44 -0800

    Use highlights.scm from tree-sitter-cpp

--- config_nvim/queries/cpp/highlights.scm
+++ config_nvim/queries/cpp/highlights.scm
@@ -1,9 +1,49 @@
+; Functions
+
+(call_expression
+  function: (qualified_identifier
+    name: (identifier) @function))
+
+(template_function
+  name: (identifier) @function)
+
+(template_method
+  name: (field_identifier) @function)
+
+(template_function
+  name: (identifier) @function)
+
+(function_declarator
+  declarator: (qualified_identifier
+    name: (identifier) @function))
+
+(function_declarator
+  declarator: (qualified_identifier
+    name: (identifier) @function))
+
+(function_declarator
+  declarator: (field_identifier) @function)
+
+; Types
+
+((namespace_identifier) @type
+ (#match? @type "^[A-Z]"))
+
 (auto) @type
+
+; Constants
+
+(this) @variable.builtin
+(nullptr) @constant
+
+; Keywords
 
 [
  "catch"
  "class"
  "co_await"
+ "co_return"
+ "co_yield"
  "constexpr"
  "constinit"
  "consteval"
@@ -28,3 +68,7 @@
  "concept"
  "requires"
 ] @keyword
+
+; Strings
+
+(raw_string_literal) @string
