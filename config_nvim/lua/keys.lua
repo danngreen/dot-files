0166462e579e4d100224302bc419b5486281f9e0
Dan Green <danngreen1@gmail.com>
2023-01-20 07:51:39 -0800

    Lazy Git

--- config_nvim/lua/keys.lua
+++ config_nvim/lua/keys.lua
@@ -141,6 +141,8 @@ vnoremap("<M-c>", '"+y')
 -- Building
 nnoremap("<leader>m", ":wa<CR>:Make!<CR>")
 
+nnoremap("<F7>", "<cmd>Git<CR>")
+nnoremap("<F19>", "<cmd>FloatermNew --height=1.0 --width=1.0 --autoclose=1 lazygit<CR>")
 
 --- Debugging
 nnoremap("<leader>dl", "<cmd>call vimspector#Launch()<cr>")
