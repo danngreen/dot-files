4535232d0d8773112e93923a74fcd271fc294bbd
Dan Green <danngreen1@gmail.com>
2023-12-28 20:35:20 -0800

    Noice config
    -do not override lsp_signature or docs. Need to fix Search results

--- config_nvim/lua/noice-conf.lua
+++ config_nvim/lua/noice-conf.lua
@@ -1,30 +1,53 @@
 local _M = {}
 
-_M.config = 
+_M.config =
 {
-	cmdline = {
-	    view = "cmdline_popup",
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
+			["config.lsp.signature.enabled"] = true,
+		},
+		signature = {
+			enabled = false,
+		}
 	},
+	messages = {
+		view_search = "notify",
+	},
+	presets = {
+		-- you can enable a preset by setting it to true, or a table that will override the preset config
+		-- you can also add custom presets that you can enable/disable with enabled=true
+		bottom_search = true, -- use a classic bottom cmdline for search
+		command_palette = false, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
 
-    routes = {
-		{
-		    filter = { event = "msg_show", kind = "search_count" },
-		    opts = { skip = true },
-		},
-		{
-		    view = "split",
-		    filter = { event = "msg_show", min_height = 20 },
-		},
-		{
-			view = "notify",
-			filter = { event = "msg_show", find = "written" },
-			opts = { skip = true },
-		},
-		{
-			filter = { event = "msg_show", find = "lines" },
-			opts = { skip = true },
-		},
-  },
+	},
+	-- cmdline = {
+	-- 	view = "cmdline_popup",
+	-- },
+	-- routes = {
+	-- 	{
+	-- 		filter = { event = "msg_show", kind = "search_count" },
+	-- 		opts = { skip = true },
+	-- 	},
+	-- 	{
+	-- 		view = "split",
+	-- 		filter = { event = "msg_show", min_height = 20 },
+	-- 	},
+	-- 	{
+	-- 		view = "notify",
+	-- 		filter = { event = "msg_show", find = "written" },
+	-- 		opts = { skip = true },
+	-- 	},
+	-- 	{
+	-- 		filter = { event = "msg_show", find = "lines" },
+	-- 		opts = { skip = true },
+	-- 	},
+	-- },
 }
 
 return _M
