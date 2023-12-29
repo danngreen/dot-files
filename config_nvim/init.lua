542afefca6e06be08c2e49e9915b0c7d0dd850c7
Dan Green <danngreen1@gmail.com>
2023-12-28 20:36:09 -0800

    Formatting

--- config_nvim/init.lua
+++ config_nvim/init.lua
@@ -31,11 +31,11 @@ vim.wo.signcolumn = "yes"
 vim.opt.shortmess:append("c") -- Avoid showing message extra message when using completion
 vim.opt.shortmess:remove("F")
 vim.o.wildmenu = true
-vim.opt.wildignore:append {"tags,tags.*,build/*"}
+vim.opt.wildignore:append { "tags,tags.*,build/*" }
 vim.o.path = ".,,**"
-vim.opt.diffopt:append {"linematch:60"}
+vim.opt.diffopt:append { "linematch:60" }
 
-vim.cmd[[set diffopt+=linematch:60]]
+vim.cmd [[set diffopt+=linematch:60]]
 
 -- Key Mappings
 require "keys"
@@ -80,7 +80,7 @@ vim.api.nvim_exec(
 	false
 )
 
-vim.cmd[[set exrc]]
+vim.cmd [[set exrc]]
 
 --Set these last, some plugin overrides them. TODO: which one?
 vim.o.formatoptions = vim.o.formatoptions .. "n" --Format lists
@@ -88,8 +88,8 @@ vim.opt.formatoptions:remove "r" -- Don't insert comment leader after pressing <
 vim.opt.formatoptions:remove "o" -- Don't insert comment leader after pressing o or O
 
 vim.on_key(function(char)
-  if vim.fn.mode() == "n" then
-    vim.opt.hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
+	if vim.fn.mode() == "n" then
+		vim.opt.hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
 	end
 end, vim.api.nvim_create_namespace "auto_hlsearch")
 
