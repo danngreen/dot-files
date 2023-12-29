dcaeb78298be3cdf7ff3bfec3c9f7566bb319f96
Dan Green <danngreen1@gmail.com>
2023-12-28 20:35:57 -0800

    set line number always on

--- config_nvim/init.lua
+++ config_nvim/init.lua
@@ -90,5 +90,7 @@ vim.opt.formatoptions:remove "o" -- Don't insert comment leader after pressing o
 vim.on_key(function(char)
   if vim.fn.mode() == "n" then
     vim.opt.hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
-  end
+	end
 end, vim.api.nvim_create_namespace "auto_hlsearch")
+
+vim.wo.number = true
