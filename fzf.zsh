01910ab70ec27d0724dfd2de88ce8c0d97c9f751
Dan Green <danngreen1@gmail.com>
2024-06-25 10:33:32 -0700

    FZF display options

--- fzf.zsh
+++ fzf.zsh
@@ -11,3 +11,12 @@ fi
 # Key bindings
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:-1,fg+:#d0d0d0,bg:-1,bg+:#702b52
+  --color=hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#87ff00
+  --color=prompt:#d7005f,spinner:#af5fff,pointer:#af5fff,header:#87afaf
+  --color=gutter:#000000,border:#262626,label:#aeaeae,query:#d9d9d9
+  --border="bold" --border-label="" --preview-window="border-rounded" --prompt="> "
+  --marker=">" --pointer="◆" --separator="─" --scrollbar="│"
+  --layout="reverse"'
