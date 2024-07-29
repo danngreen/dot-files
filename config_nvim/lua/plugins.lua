81d50352c31d51e703fd7f752ec537a037b5ebca
Dan Green <danngreen1@gmail.com>
2024-07-28 22:15:59 -0700

    Style barbar

--- config_nvim/lua/plugins.lua
+++ config_nvim/lua/plugins.lua
@@ -103,18 +103,19 @@ require("lazy").setup({
 	{
 		'romgrk/barbar.nvim',
 		dependencies = {
-			-- 'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
 			-- 'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
 		},
 		init = function() vim.g.barbar_auto_setup = false end,
 		opts = {
-			icons = { filetype = { enabled = false } },
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
+			icons = {
+				button = 'X',
+				filetype = { enabled = false }
+			},
+			focus_on_close = 'previous',
+			hide = { current = false },
 		},
-		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+		-- version = '^1.0.0', -- optional: only update when a new 1.x version is released
 	},
 
 	--
