5620b660681786f51ff3ecf993f3b55efada772a
Dan Green <danngreen1@gmail.com>
2025-02-16 20:58:36 -0800

    Update goldbolt plugin

--- config_nvim/lua/godbolt-conf.lua
+++ config_nvim/lua/godbolt-conf.lua
@@ -1,20 +1,19 @@
-local M={}
+local M = {}
 
 M.config = {
-    languages = {
-        c = { compiler = "arm1121", options = {} },
-        cpp = { compiler = "arm1121",
+	languages = {
+		c = { compiler = "arm1231", options = {} },
+		cpp = { compiler = "arm1231",
 			options = {
-			-- userArguments = "-mfloat-abi=hard -mthumb-interwork -mno-unaligned-access -funsafe-math-optimizations -Ofast -mfpu=fpv5-d16 -mcpu=cortex-m7"
-			userArguments = "-std=c++20 -mfloat-abi=hard -mthumb-interwork -mno-unaligned-access -funsafe-math-optimizations -Ofast -mvectorize-with-neon-quad -mfpu=neon-vfpv4 -mtune=cortex-a7 -mcpu=cortex-a7"
-		} },
-    },
-    quickfix = {
-        enable = true,
-        auto_open = true
-    },
-    url = "https://godbolt.org"
+				-- userArguments = "-mfloat-abi=hard -mthumb-interwork -mno-unaligned-access -funsafe-math-optimizations -Ofast -mfpu=fpv5-d16 -mcpu=cortex-m7"
+				userArguments = "-std=c++20 -mfloat-abi=hard -mthumb-interwork -mno-unaligned-access -funsafe-math-optimizations -Ofast -mvectorize-with-neon-quad -mfpu=neon-vfpv4 -mtune=cortex-a7 -mcpu=cortex-a7"
+			} },
+	},
+	quickfix = {
+		enable = true,
+		auto_open = true
+	},
+	url = "https://godbolt.org"
 }
 
 return M
-
